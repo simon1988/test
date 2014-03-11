7187add7344e470c9843918ac011d57a1ada0b5f
xn91913 <xn91913@APACCNSHZJW2557.apac.nsroot.net>
2014-03-11 16:46:51 +0800

    update collection

--- test/src/main/java/fragments/TestCollection.java
+++ test/src/main/java/fragments/TestCollection.java
@@ -23,11 +23,11 @@ public class TestCollection {
 		}
 	}
 	public void test2(){
-		LinkedList<Integer> list = (LinkedList<Integer>)Collections.synchronizedList(new LinkedList<Integer>());
+		LinkedList<Integer> list = new LinkedList<Integer>();
 		System.out.println(list.peek());
 		Collections.addAll(list, 1,2,3,4,5);
 		System.out.println(list.peek());
-		System.out.println(list);
+		System.out.println(list.peekLast());
 		
 		Stack<Integer> stack = new Stack<Integer>();
 		stack.push(2);
@@ -36,9 +36,11 @@ public class TestCollection {
 		Queue<Integer> queue = new LinkedList<Integer>();
 		queue.offer(2);
 		queue.offer(3);
+		System.out.println(queue);
 		System.out.println(queue.poll());
 	}
 	public <someg> void test3(someg...g){
+		System.out.println(g.length);
 		for(Map.Entry<String, String> entry : System.getenv().entrySet()){
 			System.out.println(entry.getKey()+" : "+entry.getValue());
 		}
@@ -49,52 +51,12 @@ public class TestCollection {
 		System.out.println(set.size()+":"+set);
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		TestCollection tc = new TestCollection();
 		//tc.test1();
-		//tc.test2();
-		//tc.test3(9,5,6);
-		tc.test4();
+//		tc.test2();
+		tc.test3(9,5,6);
+//		tc.test4();
 
 	}
 
-}
-
-class MyCollection<T> extends AbstractCollection<T>{
-
-	private T[] data;
-	
-	@Override
-	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return new Iterator<T>(){
-
-			private int i=0;
-			@Override
-			public boolean hasNext() {
-				// TODO Auto-generated method stub
-				return i<=data.length;
-			}
-
-			@Override
-			public T next() {
-				// TODO Auto-generated method stub
-				return data[i++];
-			}
-
-			@Override
-			public void remove() {
-				// TODO Auto-generated method stub
-				throw new UnsupportedOperationException();
-			}
-			
-		};
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
 }
