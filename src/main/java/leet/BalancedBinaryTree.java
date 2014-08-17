c4e46e3b6389de607beaf48f0d6b63ae25ec2acc
simon1988 <simon.niu1988@gmail.com>
2014-08-17 11:49:31 +0800

    solve b tree questions

--- src/main/java/leet/BalancedBinaryTree.java
+++ src/main/java/leet/BalancedBinaryTree.java
@@ -10,20 +10,21 @@ package leet;
  */
 
 public class BalancedBinaryTree {
-	public boolean isBalanced(TreeNode root) {
-		return determine(root) >= 0 ? true : false;
-	}
+	private int getBalancedTreeHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = getBalancedTreeHeight(root.left);
+        int r = getBalancedTreeHeight(root.right);
+        if (l >= 0 && r >= 0) {
+            if (Math.abs(l - r) <= 1) {
+                return Math.max(l, r) + 1;
+            }
+        }
+        return -1;
+    }
 
-	private int determine(TreeNode root) {
-		if (root == null) {
-			return 0;
-		} else {
-			int leftDepth = determine(root.left);
-			int rightDepth = determine(root.right);
-			if (leftDepth < 0 || rightDepth < 0
-					|| Math.abs(leftDepth - rightDepth) > 1)
-				return -1;
-			return Math.max(leftDepth, rightDepth) + 1;
-		}
-	}
+    public boolean isBalanced(TreeNode root) {
+        return getBalancedTreeHeight(root) >= 0;
+    }
 }
--- src/main/java/leet/BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/leet/BinaryTreeLevelOrderTraversalII.java
@@ -2,8 +2,7 @@ package leet;
 
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Collections;
 
 /** 
  * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
@@ -26,42 +25,25 @@ import java.util.Queue;
  */
 
 public class BinaryTreeLevelOrderTraversalII {
-	public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
-		ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
-		if (root == null)
-			return ret;
-		ArrayList<Integer> level = new ArrayList<Integer>();
-		Queue<TreeNode> queue = new LinkedList<TreeNode>();
-		queue.add(root);
-		int currentLevel = 1;
-		int nextLevel = 0;
-		while (!queue.isEmpty()) {
-			TreeNode node = queue.remove();
-			level.add(node.val);
-			currentLevel--;
-			if (node.left != null) {
-				queue.add(node.left);
-				nextLevel++;
-			}
-			if (node.right != null) {
-				queue.add(node.right);
-				nextLevel++;
-			}
-			if (currentLevel == 0) {
-				ret.add(level);
-				level = new ArrayList<Integer>();
-				currentLevel = nextLevel;
-				nextLevel = 0;
-			}
-		}
-		int i = 0, j = ret.size() - 1;
-		while (i < j) {
-			ArrayList<Integer> tmp = ret.get(i);
-			ret.set(i, ret.get(j));
-			ret.set(j, tmp);
-			i++;
-			j--;
-		}
-		return ret;
-	}
+	public class Solution {
+        private void search(TreeNode root, int level,
+                ArrayList<ArrayList<Integer>> ans) {
+            if (root == null) {
+                return;
+            }
+            if (level >= ans.size()) {
+                ans.add(new ArrayList<Integer>());
+            }
+            ans.get(level).add(root.val);
+            search(root.left, level + 1, ans);
+            search(root.right, level + 1, ans);
+        }
+
+        public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+            ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+            search(root, 0, ans);
+            Collections.reverse(ans);
+            return ans;
+        }
+    }
 }
