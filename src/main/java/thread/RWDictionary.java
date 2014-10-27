081ab103b2b740cebc1d4413187da4c002c47433
simon1988 <simon.niu1988@gmail.com>
2014-10-27 10:37:00 +0800

    readwritelock impl

---
+++ src/main/java/thread/RWDictionary.java
@@ -0,0 +1,50 @@
+package thread;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class RWDictionary {
+	private final Map<String, String> m = new TreeMap<String, String>();
+	private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+	private final Lock r = rwl.readLock();
+	private final Lock w = rwl.writeLock();
+
+	public String get(String key) {
+		r.lock();
+		try {
+			return m.get(key);
+		} finally {
+			r.unlock();
+		}
+	}
+
+	public Set<String> allKeys() {
+		r.lock();
+		try {
+			return m.keySet();
+		} finally {
+			r.unlock();
+		}
+	}
+
+	public String put(String key, String value) {
+		w.lock();
+		try {
+			return m.put(key, value);
+		} finally {
+			w.unlock();
+		}
+	}
+
+	public void clear() {
+		w.lock();
+		try {
+			m.clear();
+		} finally {
+			w.unlock();
+		}
+	}
+}
