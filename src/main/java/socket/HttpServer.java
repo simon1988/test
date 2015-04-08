97324ee9e019a53391ba2f5489fb3cb4bf8b9410
niuxingman <niuxingman@corp.netease.com>
2015-04-08 17:23:34 +0800

    Add Netty echo server

---
+++ src/main/java/socket/HttpServer.java
@@ -0,0 +1,10 @@
+package socket;
+
+/**
+ * 
+ * @see HttpHelloWorldServer
+ * https://github.com/netty/netty/blob/master/example/src/main/java/io/netty/example/http/helloworld/HttpHelloWorldServer.java
+ */
+public class HttpServer {
+
+}
---
+++ src/main/java/socket/NettyEchoServer.java
@@ -0,0 +1,88 @@
+package socket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.CharsetUtil;
+
+public class NettyEchoServer {
+	private int port;
+	private EventLoopGroup bossGroup;
+	private EventLoopGroup workerGroup;
+
+    public NettyEchoServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        bossGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .childHandler(new ChannelInitializer<SocketChannel>() {
+                 @Override
+                 public void initChannel(SocketChannel ch) throws Exception {
+                     ch.pipeline().addLast(new LineBasedFrameDecoder(64),new StringDecoder(CharsetUtil.ISO_8859_1),new EchoServerHandler());
+                 }
+             })
+             .option(ChannelOption.SO_BACKLOG, 128)
+             .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind(port).sync();
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new NettyEchoServer(port).run();
+    }
+    
+	private class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+	    @Override
+	    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+	    	String text = (String)msg;
+	    	if("exit".equalsIgnoreCase(text)){
+	    		text=text+"!";
+	    	}
+	    	text=text+"\n";
+	    	ByteBuf encoded = ctx.alloc().buffer(text.length());
+	    	encoded.writeBytes(text.getBytes(CharsetUtil.ISO_8859_1));
+	    	ctx.write(encoded);
+	        ctx.flush();
+	    }
+
+	    @Override
+	    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+	        // Close the connection when an exception is raised.
+	        cause.printStackTrace();
+	        ctx.close();
+	    }
+	}
+}
