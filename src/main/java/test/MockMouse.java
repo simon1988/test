6b4938017df88c0a95b6a6250b3c7080436e4c74
simon1988 <simon.niu1988@gmail.com>
2015-11-14 09:01:22 +0800

    mock mouse +

--- src/main/java/test/MockMouse.java
+++ src/main/java/test/MockMouse.java
@@ -1,41 +1,41 @@
 package test;
 
-import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 public class MockMouse {
 
-	public static void main(String[] args) {
-		try {
-			Thread.sleep(5000);
-			final Robot rb = new Robot();
-			while (true)
-				pressMouse(rb, InputEvent.BUTTON1_MASK, 3000);
-//			int[] ks = new int[] { KeyEvent.VK_E, KeyEvent.VK_X, KeyEvent.VK_I, KeyEvent.VK_T, KeyEvent.VK_ENTER };
-//			pressKeys(rb, ks, 500);
-		} catch (AWTException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
+	public static void main(String[] args) throws Exception{
+		Thread.sleep(15000);
+		Robot r = new Robot();
+		mxdWK(r);
 	}
-
-	private static void pressMouse(Robot r, int m, int delay) {
-		r.mousePress(m);
-		r.delay(10);
-		r.mouseRelease(m);
-		r.delay(delay);
-	}
-
-	private static void pressKeys(Robot r, int[] ks, int delay) {
-		for (int i = 0; i < ks.length; i++) {
-			r.keyPress(ks[i]);
+	private static void holdKey(Robot r, int key, int delay){
+		long cur = System.currentTimeMillis();
+		while(System.currentTimeMillis()-cur<delay){
+			r.keyPress(key);
 			r.delay(10);
-			r.keyRelease(ks[i]);
-			r.delay(delay);
 		}
+		r.keyRelease(key);
 	}
+	private static void mxdWK(Robot r) throws Exception{
+		while(true){
+			holdKey(r, KeyEvent.VK_RIGHT, 3000);
+			r.keyPress(KeyEvent.VK_CONTROL);
+			r.delay(3000);
+			r.keyRelease(KeyEvent.VK_CONTROL);
+			r.keyPress(KeyEvent.VK_SPACE);
+			r.delay(3000);
+			r.keyRelease(KeyEvent.VK_SPACE);
+			pressMouse(r);
+		}
+		
+	}
+	private static void pressMouse(Robot r) throws Exception{
+		r.mousePress(InputEvent.BUTTON1_MASK);
+		r.delay(10);
+		r.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+
 }
