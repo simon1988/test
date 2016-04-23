f90e5364edb4aaae420eebd1e314ba65783ad40f
simon <simon@windows10.microdone.cn>
2016-04-23 18:18:10 +0800

    storm wordcount

---
+++ src/main/java/storm/TopologyMain.java
@@ -0,0 +1,34 @@
+package storm;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+
+import storm.bolts.WordCounter;
+import storm.bolts.WordNormalizer;
+import storm.spouts.WordReader;
+
+public class TopologyMain {
+	public static void main(String[] args) throws InterruptedException {
+         
+        //Topology definition
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("word-reader",new WordReader());
+		builder.setBolt("word-normalizer", new WordNormalizer())
+			.shuffleGrouping("word-reader");
+		builder.setBolt("word-counter", new WordCounter(),4)
+			.fieldsGrouping("word-normalizer", new Fields("word"));
+		
+        //Configuration
+		Config conf = new Config();
+		conf.put("wordsFile", Thread.currentThread().getContextClassLoader().getResource("bible.txt").getPath());
+		conf.setDebug(false);
+        //Topology run
+		conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("Getting-Started-Toplogie", conf, builder.createTopology());
+		Thread.sleep(40000);
+		cluster.shutdown();
+	}
+}
---
+++ src/main/java/storm/bolts/WordNormalizer.java
@@ -0,0 +1,40 @@
+package storm.bolts;
+
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+public class WordNormalizer extends BaseBasicBolt {
+
+	public void cleanup() {}
+
+	/**
+	 * The bolt will receive the line from the
+	 * words file and process it to Normalize this line
+	 * 
+	 * The normalize will be put the words in lower case
+	 * and split the line to get all words in this 
+	 */
+	public void execute(Tuple input, BasicOutputCollector collector) {
+        String sentence = input.getString(0);
+        String[] words = sentence.split(" ");
+        for(String word : words){
+            word = word.trim().replaceAll("[,:.;]", "");
+            if(!word.isEmpty()){
+                word = word.toLowerCase();
+                collector.emit(new Values(word));
+            }
+        }
+	}
+	
+
+	/**
+	 * The bolt will only emit the field "word" 
+	 */
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("word"));
+	}
+}
