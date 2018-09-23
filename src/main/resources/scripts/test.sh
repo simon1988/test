b955c5267244eac693480e6939f7a7a8d0411f2c
simon1988 <simon.niu1988@gmail.com>
2018-09-23 11:20:17 +0800

    update shell scripts

--- src/main/resources/scripts/test.sh
+++ src/main/resources/scripts/test.sh
@@ -1,5 +1,8 @@
 #shell
 for file in `ls`;do sed -i -E 's/(.*com) (.*)/\2 \1/' $file; done
+tail tmp.txt |cut -c1-19|grep "2018-05-29"|sort -k2|uniq -c
+tail tmp.txt |sed -E 's/.*userId=(2266[0-9]+);.*/\1/'|sort -k1|uniq -c
+cat 2.txt |awk '{print $1}'|sort -k1|uniq -c|sort -nk2
 
 #git
 git reset --hard <commit_id>
@@ -12,3 +15,6 @@ git merge --squash <branch> 多次commit合成一个
 git format-patch A..B --> git am *.patch
 git cherry-pick develop/cherry-pick A..B
 
+git tag -a 1.5.1 -m 'WebSite version 1.5.1'
+git push origin :refs/tags/1.5.1
+git push origin 1.5.1
